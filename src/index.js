bd2f2f4ceb715ad09dda80bae0c3df6f4f2432ef
mummick <mummick6@gmail.com>
2021-02-26 13:32:32 +0300

    feat: task 'Typical array problems' solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    return array.reduce(
+        (accumulator, currentValue) =>
+            currentValue < accumulator ? currentValue : accumulator,
+        Infinity
+    );
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return array.reduce(
+        (accumulator, currentValue) =>
+            currentValue > accumulator ? currentValue : accumulator,
+        -Infinity
+    );
+};
+
+exports.avg = function avg(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+
+    return (
+        array.reduce(
+            (accumulator, currentValue) => accumulator + currentValue,
+            0
+        ) / array.length
+    );
+};
